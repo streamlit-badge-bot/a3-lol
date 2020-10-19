8d4579a5a375dcf563265f5d32bb18c5dee16c0b
ShaoboGuan <shaobog@andrew.cmu.edu>
2020-10-18 21:41:47 -0400

    Basic visualizations

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,28 +2,62 @@ import streamlit as st
 import pandas as pd
 import altair as alt
 
-st.title("Let's analyze some Penguin Data 🐧📊.")
+st.title("Let's analyze some Data about Majors and Jobs!")
 
 @st.cache  # add caching so we load the data only once
 def load_data():
-    # Load the penguin data from https://github.com/allisonhorst/palmerpenguins.
-    penguins_url = "https://raw.githubusercontent.com/allisonhorst/palmerpenguins/v0.1.0/inst/extdata/penguins.csv"
-    return pd.read_csv(penguins_url)
+    # Load the Major data from https://raw.githubusercontent.com/fivethirtyeight/data/master/college-majors/recent-grads.csv.
+    recent_grad_url = "https://raw.githubusercontent.com/fivethirtyeight/data/master/college-majors/recent-grads.csv"
+    return pd.read_csv(recent_grad_url)
 
 df = load_data()
 
-st.write("Let's look at raw data in the Pandas Data Frame.")
+# st.write("Let's look at raw data in the Pandas Data Frame.")
 
-st.write(df)
+# st.write(df)
 
-st.write("Hmm 🤔, is there some correlation between body mass and flipper length? Let's make a scatterplot with [Altair](https://altair-viz.github.io/) to find.")
+st.write("Here is the visualization you can play with!")
 
-chart = alt.Chart(df).mark_point().encode(
-    x=alt.X("body_mass_g", scale=alt.Scale(zero=False)),
-    y=alt.Y("flipper_length_mm", scale=alt.Scale(zero=False)),
-    color=alt.Y("species")
+# side bar #
+useful_cols = [df.columns[i] \
+               for i in range(len(df.columns)) if i not in [0,1,2,6]]
+
+option_field = st.sidebar.selectbox(
+    'Choose a field!',
+     useful_cols)  
+
+explanations = { "Total"        :"Total number of people with major",
+                "Sample_size"   :"Sample size (unweighted) of full-time, year-round ONLY (used for earnings)",
+                "Men"           :"Male graduates",
+                "Women"         :"Female graduates",
+                "ShareWomen"    :"Women as share of total",
+                "Employed"      :"Number employed",
+                "Full_time"     :"Employed 35 hours or more",
+                "Part_time"     :"Employed less than 35 hours",
+                "Full_time_year_round":"Employed at least 50 weeks and at least 35 hours",
+                "Unemployed"    :"Number unemployed",
+                "Unemployment_rate":"Unemployed / (Unemployed + Employed)",
+                "Median"        :"Median earnings of full-time, year-round workers",
+                "P25th"         :"25th percentile of earnings",
+                "P75th"         :"75th percentile of earnings",
+                "College_jobs"  :"Number with job requiring a college degree",
+                "Non_college_jobs":"Number with job not requiring a college degree",
+                "Low_wage_jobs" :"Number in low-wage service jobs"
+                }
+
+st.sidebar.write(explanations[option_field])
+
+# side bar end #
+
+# first visualization #
+chart = alt.Chart(df).mark_bar().encode(
+    x=alt.X(option_field, scale=alt.Scale(zero=False),),
+    y=alt.Y("Major", scale=alt.Scale(zero=False), sort = '-x'),
+    color=alt.Y("Major_category"),
+    tooltip=["Major", "Major_category", option_field]
 ).properties(
-    width=600, height=400
-).interactive()
+    width=1500, height=2000
+)
 
 st.write(chart)
+# first visualization end #
