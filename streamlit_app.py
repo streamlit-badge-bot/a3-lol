1fc15a855eebf70f779eae9d9dfe7926b36393fe
ShaoboGuan <shaobog@andrew.cmu.edu>
2020-10-20 14:19:45 -0400

    Add a support charts for visual 2

--- streamlit_app.py
+++ streamlit_app.py
@@ -79,6 +79,7 @@ def draw_major_statistics(df, selected, useful_cols):
 
 def draw_correlations(df, selected, useful_cols):
     # second visualization #
+    
     st.header("Try to see some correlations between two statistics of your choice!")
     filtered_df = df.loc[df["Major_category"].isin(selected)]
     option_field_x = st.selectbox(
@@ -89,21 +90,42 @@ def draw_correlations(df, selected, useful_cols):
         'Choose a field for the y-axis!',
          useful_cols)
 
-    visual_2 = alt.Chart(filtered_df).mark_circle().encode(
-        x=alt.X(option_field_x,
+    brush = alt.selection(type = 'interval')
+
+    corr = alt.Chart(filtered_df).mark_point().encode(
+        x=alt.X(option_field_x+":Q",
                 scale=alt.Scale(zero=False),
                 axis=alt.Axis(labelOverlap=True),
                 ),
-        y=alt.Y(option_field_y,
+        y=alt.Y(option_field_y+":Q",
                 scale=alt.Scale(zero=False),
                 axis=alt.Axis(labelOverlap=True),
                 ),
+        opacity=alt.condition(brush, alt.OpacityValue(1), alt.OpacityValue(0.1)),
         color=alt.Y("Major_category"),
         tooltip=["Major", option_field_y, option_field_x]
+    ).add_selection(
+        brush
     ).properties(
-        width=1000, height=750
+        width=400, height=400
     )
 
+    support_chart = alt.Chart(filtered_df).mark_bar().encode(
+        x=alt.X("Major", scale=alt.Scale(zero=False),
+                sort = '-y',
+                axis=alt.Axis(labelOverlap=True)),
+        color=alt.Y("Major_category"),
+        opacity=alt.condition(brush, alt.OpacityValue(1), alt.OpacityValue(0.4)),
+    ).add_selection(
+        brush
+    ).properties(
+        width=400, height=400
+    )        
+        
+    visual_2 = corr | \
+        support_chart.encode(y=alt.Y(option_field_x, scale=alt.Scale(zero=False))) | \
+            support_chart.encode(y=alt.Y(option_field_y, scale=alt.Scale(zero=False)))
+            
     st.write(visual_2)
     # second visualization end #
 
